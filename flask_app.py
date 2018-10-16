e424fd51ed8daca386c085230cfff5482414b677
Leo Febey <overclockedamiga@gmail.com>
2018-10-16 08:08:05 +0000

    Added flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['host'] = '0.0.0.0'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/js/<path:path>')
+def js_files(path):
+    return send_from_directory('html/js', path)
+
+@app.route('/css/<path:path>')
+def css_files(path):
+    return send_from_directory('html/css', path)
+
+@app.route('/images/<path:path>')
+def image_files(path):
+    return send_from_directory('html/img', path)
+
+if __name__ == '__main__':
+    socketio.run(app)
